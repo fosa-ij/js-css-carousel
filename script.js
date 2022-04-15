32b42289d19f37f01bb1c50e8a32163f6d7e105a
fosa-ij <efosaij@gmail.com>
2022-04-15 23:23:32 +0100

    make carousel working

--- script.js
+++ script.js
@@ -16,3 +16,52 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+
+//create a list of images 
+//set the appropriate classes on the lists
+// loop through the list changing the src attribute on them by looping through the images gotten from the server.
+
+
+
+// let n = [];
+// let m = [];
+
+// n.forEach((num1, index) => {
+//   const num2 = m[index];
+//   console.log(num1, num2);
+// });
+
+
+
+
+
+document.querySelector('#button').addEventListener('click', getCocktail = () => {
+  const drink = document.querySelector('#drink').value
+  let drinkCocktail = drink.replaceAll(' ', '%20')
+   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkCocktail}`)
+  //  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+   .then(res => res.json()) // parse response as JSON
+   .then(data => {
+       console.log(data.drinks)
+       data.drinks.forEach((x, i) => {
+        let li = document.createElement('li')
+        li.className = 'slide'
+        if (i === 0) {
+          let classLi = document.createAttribute('data-active')
+          li.setAttributeNode(classLi)
+        }
+        let img = document.createElement('img')
+        img.src = data.drinks[i].strDrinkThumb
+        // let src = document.createAttribute('src').value = data.drinks[i].strDrinkThumb
+        // img.setAttributeNode(src)
+        li.appendChild(img)
+        document.querySelector('ul').appendChild(li)
+      
+      })
+   })
+   .catch(err => {
+       console.log(`error ${err}`)
+   });
+      //  document.querySelector('button').classList.toggle('hidden')
+})
